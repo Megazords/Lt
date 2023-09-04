7f66916bc23421e5d750d13ac808802b69e0798a
MEGA-DESKTOP\Mega <carlospalheiros@gmail.com>
2023-09-04 20:25:53 +0100

    Inicial commit + Navbar

---
+++ src/app/page.tsx
@@ -0,0 +1,13 @@
+'use client';
+
+import { Box } from '@chakra-ui/react';
+
+const Home: React.FunctionComponent = () => {
+  return (
+    <Box height="100vh" width="100vw" backgroundColor="black">
+      vodka{' '}
+    </Box>
+  );
+};
+
+export default Home;
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,46 @@
+'use client';
+import { SearchIcon } from '@chakra-ui/icons';
+import { Flex, Icon, Image, Link, Text } from '@chakra-ui/react';
+import { GrSearch } from 'react-icons/gr';
+
+const Navbar: React.FunctionComponent = () => {
+  return (
+    <Flex
+      color="white"
+      width="100vw"
+      position="fixed"
+      top="0"
+      alignItems="center"
+      padding="1rem"
+      justifyContent="space-between">
+      <Link display="flex" alignItems="center" href="/">
+        <Image src="Lt_logo.png" height="56px"></Image>
+        <Text
+          fontSize="24px"
+          paddingLeft="12px"
+          fontWeight="bold"
+          fontFamily="Montserrat">
+          luztuna.pt
+        </Text>
+      </Link>
+      <Flex width="30%" justifyContent="space-evenly">
+        <Link href="/">
+          <Text fontFamily="Montserrat">Página Inicial</Text>
+        </Link>
+        <Link href="/membros">
+          <Text fontFamily="Montserrat">Membros</Text>
+        </Link>
+        <Link href="/palmares">
+          <Text fontFamily="Montserrat">Palmarés</Text>
+        </Link>
+        <Link href="/contactos">
+          <Text fontFamily="Montserrat">Contactos</Text>
+        </Link>
+
+        <SearchIcon></SearchIcon>
+      </Flex>
+    </Flex>
+  );
+};
+
+export default Navbar;
