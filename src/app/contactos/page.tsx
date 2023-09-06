b390a4cb52f39b3fec511fd2ae3ab138d1a11773
MEGA-DESKTOP\Mega <carlospalheiros@gmail.com>
2023-09-06 16:34:54 +0100

    added images to folder, fixed parallax box size

--- src/app/contactos/page.tsx
+++ src/app/contactos/page.tsx
@@ -7,7 +7,7 @@ const Home: React.FunctionComponent = () => {
   return (
     <>
       <Box
-        height="75vh"
+        height="33vh"
         width="100%"
         overflow="hidden"
         _after={{
@@ -17,21 +17,26 @@ const Home: React.FunctionComponent = () => {
           left: 0,
           width: '100%',
           height: '100%',
-          backgroundColor: 'rgba(0, 0, 0, 0.2)', // Adjust the opacity by changing the alpha value (0.5 for 50% opacity), // Ensure the pseudo-element doesn't interfere with interactions
+          backgroundColor: 'rgba(0, 0, 0, 0.1)', // Adjust the opacity by changing the alpha value (0.5 for 50% opacity), // Ensure the pseudo-element doesn't interfere with interactions
         }}>
         <Image src="Lt_foto.jpg" objectFit="cover" width="100%"></Image>
       </Box>
       <Parallax>
         <Flex
           direction="column"
+          border="20px"
+          padding="16px"
+          borderColor="black"
           height="50vh"
           alignItems="center"
           justifyContent="center"
           backgroundColor="white">
-          <Text fontFamily="Montserrat" padding="0 0 1rem 0">
+          <Text fontFamily="Montserrat" padding="0 0 1rem 0" color="black">
             963 330 001
           </Text>
-          <Text fontFamily="Montserrat">luz.tuna@gmail.com</Text>
+          <Text fontFamily="Montserrat" color="black">
+            luz.tuna@gmail.com
+          </Text>
         </Flex>
       </Parallax>
     </>
