3223dfbac28d194848cd510c94008166346ecc71
MEGA-DESKTOP\Mega <carlospalheiros@gmail.com>
2023-09-10 17:50:08 +0100

    change color palate, fixed distance and reactiveness

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,33 +4,44 @@ import { Flex, Image, Link, Text } from '@chakra-ui/react';
 const Navbar: React.FunctionComponent = () => {
   return (
     <Flex
-      textShadow="0px 0px 2px rgba(0,0,0,1)"
-      color="white"
-      width="100vw"
+      textShadow="0px 0px 1px rgba(0,0,0,0.8)"
+      color="black"
+      backgroundColor="white"
+      width={['100%', '100%', '800px', '15000px']}
+      maxWidth="100%"
       position="fixed"
       top="0"
       alignItems="center"
       padding="1rem"
-      justifyContent="space-between">
+      justifyContent={['center', 'space-between']}>
       <Link display="flex" alignItems="center" href="/">
-        <Image src="Lt_logo.png" height="56px"></Image>
+        <Image src="Lt_logo.png" height={['36px', '48px', '56px']}></Image>
         <Text
-          fontSize="24px"
+          fontSize={['18px', '20px', '24px']}
           paddingLeft="12px"
           fontWeight="bold"
           fontFamily="Montserrat">
           luztuna.pt
         </Text>
       </Link>
-      <Flex width="30%" justifyContent="space-evenly">
+      <Flex
+        minWidth={['100%', '100%', '400px']}
+        width={['100%', '100%', '40%']}
+        justifyContent={['center', 'space-evenly']}>
         <Link href="/">
-          <Text fontFamily="Montserrat">Página Inicial</Text>
+          <Text fontFamily="Montserrat" margin={['0', '0', '0 5px']}>
+            Página Inicial
+          </Text>
         </Link>
         <Link href="/membros">
-          <Text fontFamily="Montserrat">Membros</Text>
+          <Text fontFamily="Montserrat" margin={['0', '0', '0 10px']}>
+            Membros
+          </Text>
         </Link>
         <Link href="/palmares">
-          <Text fontFamily="Montserrat">Palmarés</Text>
+          <Text fontFamily="Montserrat" margin={['0', '0', '0 10px']}>
+            Palmarés
+          </Text>
         </Link>
         <Link href="/contactos">
           <Text fontFamily="Montserrat">Contactos</Text>
